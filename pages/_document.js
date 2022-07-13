12dfa4dbbaea9de92da148b841c77eb3441a1757
shoxie <sktt1lka@gmail.com>
2022-07-13 08:24:28 +0700

    test

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet" />
+                
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
