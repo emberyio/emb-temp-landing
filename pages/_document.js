80df79fe6a5cba6e7c2b82625d615a6d0fe957ca
shoxie <sktt1lka@gmail.com>
2022-07-14 14:56:33 +0700

    edited with no fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet" />
-                
+        <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet" />
+        <Script src="//embed.typeform.com/next/embed.js"></Script>
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
 import { AiOutlineArrowRight, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai'
+import Typewriter from 'typewriter-effect';
+import { PopupButton } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-between h-screen px-5 lg:px-10'>
+    <div className='flex flex-col justify-between h-screen px-5 bg-black lg:px-10'>
       <div className='flex items-end justify-end w-full pt-5 pr-5'>
-        <div className='flex items-center justify-center space-x-2 text-text'>
-          <span className='font-thin uppercase'>Start a conversation</span>
-          <AiOutlineArrowRight />
-        </div>
+        <PopupButton id="LUTUxwcD" style={{ fontSize: 20 }} className="my-button">
+          <div className='flex items-center justify-center space-x-2'>
+            <span className='font-thin text-gray-400'>start a conversation</span>
+            <AiOutlineArrowRight className="text-text" />
+          </div>
+        </PopupButton>
       </div>
       <div>
-        <h1 className='font-bold text-text uppercase lg:text-[218px] text-[68px] leading-[54px] lg:leading-[174px] md:text-[129px] md:leading-[103px]'>
-          Embark on your digital journey
+        <h1 className='font-bold text-text uppercase lg:text-[150px] text-[68px] leading-[54px] lg:leading-[120px] md:text-[129px] md:leading-[103px]'>
+          embark<br />on <Typewriter
+            options={{
+              strings: ['graphic design', 'branding', 'custom software consultancy', 'software consultancy'],
+              autoStart: true,
+              loop: true,
+
+            }}
+          />
         </h1>
         <div className="pt-10">
-          <p className="text-2xl font-semibold leading-6 uppercase text-text opacity-70">We will take care of branding, digital graphic design and full-stack website development to let you<br/>focus on your business.</p>
+          <p className="text-2xl font-semibold leading-6 uppercase text-text opacity-70">We will take care of branding, digital graphic design and full-stack website development to let you<br />focus on your business.</p>
         </div>
       </div>
       <div className='flex flex-row justify-end w-full pb-10 space-x-5 text-2xl'>
